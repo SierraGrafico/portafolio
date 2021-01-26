271ad8f90de6d1190126c5b58724bcdd3cf3123d
Sierra Grafico <sierragrafico@gmail.com>
2021-01-25 21:02:43 -0500

    menu inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // menu
+$(document).ready(function() {
+    $(".menu").click(function() {
+        $(".full-menu").toggleClass("activar");
+    });
+});
+
+function stateMenu() {
+    var x = document.querySelector(".menuTxt");
+    if (x.innerHTML === "Menu") {
+        x.innerHTML = "Cerrar";
+    } else {
+        x.innerHTML = "Menu";
+    }
+}
