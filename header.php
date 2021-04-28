b3497d6fba245cf84c56c471057557b6f5000877
Sierra Grafico <sierragrafico@gmail.com>
2021-04-28 12:03:37 -0500

    28 abril

--- header.php
+++ header.php
@@ -4,9 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- SEO  -->
+     <title> Sierra Front.D con D de desarrollo y diseño</title>
+    <meta name="description" content="Portafolio Johnny Enrique Sierra Front Web Designer, Bogotá Colombia">
+    <meta name="keywords" content="HTML5, CSS3, SCSS, SASS, Javascript, diseño grafico, diseño web, frontend, diseñador, bogota, diseño, photoshop, illustrator, visual studio code, jquery">
     <!-- Favico  -->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-    <title> Sierra Front.D con D de Desarrollo y Diseño</title>
     <!-- bootstrap estilos -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
